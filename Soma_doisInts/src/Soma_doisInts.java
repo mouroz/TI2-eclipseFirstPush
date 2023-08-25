e3b1684328ebc1ace468099be9bb0cfbccd9dab1
happy <happy@192.168.0.140>
2023-08-25 00:52:02 -0300

    Initial Commit

---
+++ Soma_doisInts/src/Soma_doisInts.java
@@ -0,0 +1,16 @@
+
+import java.util.*;
+public class Soma_doisInts{
+	public static Scanner sc = new Scanner(System.in);
+	public static void main (String args[]) {
+		int n1, n2, sum;	
+		System.out.println("Digite um numéro");
+		n1 = sc.nextInt();
+		System.out.println("Digite um numéro");
+		n2 = sc.nextInt();
+		
+		sum = n1+n2;
+		
+		System.out.println("Soma = "+sum);
+	}
+}
